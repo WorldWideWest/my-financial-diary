241b5f1bc154301a4c1543ac5e5b7a7afaedbfef
Dženan Džafić <dzeno.dzafic@windowslive.com>
2022-10-17 07:40:30 +0200

    Started working on monthly reporter

--- main/reporters/monthly_module.py
+++ main/reporters/monthly_module.py
@@ -1,9 +1,22 @@
 import streamlit as st
 import pandas as pd
 
+from main.utils.filter import Filter
+
+filter = Filter()
+
 
 def monthly(data: pd.DataFrame, month: int):
+    processed = data.copy()
+    
+    processed["Date"] =  pd.to_datetime(processed["Date"], format="%m/%d/%Y")
+
+    processed = processed[(processed["Date"].dt.month == month)]
+
+    data_by_category = filter.group_by_category(processed)
 
 
 
-    return 1
+
+
+    return data_by_category
