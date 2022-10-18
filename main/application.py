fe13b882f2ca2db4bd3c6d02bb1e8ddb5ca15736
Dženan Džafić <dzeno.dzafic@windowslive.com>
2022-10-18 19:12:25 +0200

    Deleting everything and startin again

--- main/application.py
+++ main/application.py
@@ -3,21 +3,24 @@ import pandas as pd
 import streamlit as st
 
 from .utils.get_data import fetch
+from .components.form.form import form
+from .static.static_data import MONTHS, TABS
 
 
 def run():
-    data = fetch("data")
+    data = fetch(0)
+    static_data = fetch(2)
+    
 
-    static_data = fetch("static-data")
 
     CURRENT_WEEK = datetime.datetime.today().isocalendar().week
     CURRENT_MONTH = datetime.datetime.today().month
-    MONTHS = {
-        1: "January", 2: "February", 3: "March", 4: "April", 5: "May", 6: "June", 7: "July",
-        8: "August", 9: "September", 10: "October", 11: "November", 12: "December"
-    }
 
-    tabs = st.tabs(["Weekly", "Monthly", "Yarly", "Total"])
+    print(data)
+
+    static_data.to_csv("static_data.csv")
+
+    tabs = st.tabs(TABS)
 
     with st.sidebar:
         CURRENT_WEEK = st.number_input(
@@ -28,7 +31,7 @@ def run():
         CURRENT_MONTH = st.selectbox(
             label = "Select the month",
             options = list(MONTHS.keys()),
-            index = CURRENT_MONTH - 1,
+            index = CURRENT_MONTH,
             format_func = lambda x:MONTHS[ x ]
         )
 
@@ -44,6 +47,7 @@ def run():
     with tabs[1]:
         from .reporters.monthly_module import monthly
         monthly_data = monthly(data, 9)
-
         st.dataframe(monthly_data)
+        
+        form_component = form(static_data)
 
--- main/utils/filter.py
+++ main/utils/filter.py
@@ -7,3 +7,7 @@ class Filter(object):
     def group_by_category(self, data: pd.DataFrame) -> pd.DataFrame:
         data_by_category = data.groupby("Category").sum()
         return data_by_category[["Amount"]]
+
+    @st.cache
+    def sum_by_month(data: pd.DataFrame, month: str) -> float:
+        return data[(data["Month"] == month)]
