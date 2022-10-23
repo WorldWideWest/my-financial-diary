03a10e20d157e86120d28679ef21cc655b1a9f98
Dženan Džafić <dzeno.dzafic@windowslive.com>
2022-10-23 15:37:39 +0200

    Implementing the heatmap into the streamlit app and making cosmetic changes

--- main/sites/monthly/report.py
+++ main/sites/monthly/report.py
@@ -1,23 +1,57 @@
 import streamlit as st
 import pandas as pd
+import math
+import plotly.graph_objects as go
 
+from main.static.components.data import MONTHS
 
-def monthly(data: pd.DataFrame, selected_month: int):
-    
-    # Sum total spendings for each week in the year
-    # Assign the week to the months
-    # Build a heatmap
+@st.cache()
+def get_weekly_spendings_heatmap(data: pd.DataFrame) -> go.Heatmap:
 
     data["Date"] = pd.to_datetime(data["Date"], format="%m/%d/%Y")
 
-    weekly_spendings = pd.DataFrame(
-        columns = range(1, 53),
-        rows = []
+    processed = data.copy()
+    processed["Month"] = processed["Date"].dt.month_name()
+    processed["Week"] = pd.to_numeric(processed["Date"].dt.day / 7).apply(lambda x: math.ceil(x))
+
+    months = pd.DataFrame()
+
+    for month in range(1, 13):
+
+        aggregated_weeks = processed[processed["Month"] == MONTHS[month]].groupby("Week").sum("Amount")
+        aggregated_weeks = pd.Series(aggregated_weeks["Amount"].tolist(), index = aggregated_weeks.index)
+            
+        months[MONTHS[month]] = aggregated_weeks
+
+    heatmap_object = {
+        "z": months.values.tolist(),
+        "x": months.columns.tolist(),
+        "y": months.index.tolist()
+    }
+
+    hovertemplate = "Month: %{x},<br>Week: %{y},<br>Spending: %{z:.2f} KM<extra></extra>"
+    texttemplate = "%{z:.2f}"
+
+    figure = go.Figure(data = go.Heatmap(
+        heatmap_object,
+        colorscale = "magma",
+        hovertemplate = hovertemplate,
+        texttemplate = texttemplate,
+    ))
+
+    figure.update_layout(
+        xaxis = dict(showgrid = False),
+        yaxis = dict(showgrid = False)
     )
 
+    return figure
+
+def monthly(data: pd.DataFrame, selected_month: int):
     
-
-
-
+    st.plotly_chart(get_weekly_spendings_heatmap(data), use_container_width = True)
 
     return
+
+
+
+    
