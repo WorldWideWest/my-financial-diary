ebbe2b9d1ba09a0b2aaac7cb4ca7a342e5d0b5a1
Dženan Džafić <dzeno.dzafic@windowslive.com>
2022-10-24 06:21:30 +0200

    Filter improvements and adjusting those in other sites

--- main/sites/main/site.py
+++ main/sites/main/site.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import datetime as dt
+import math
 
 from main.components.tab.component import tabs 
 from main.components.selectbox.component import selectbox
@@ -13,21 +14,28 @@ def main_site(data: pd.DataFrame):
     selected_month, selected_week = None, None
 
     with st.sidebar:
+        select_year = st.selectbox(
+            "Select the Year",
+            options = range(2022, 2024),
+            index = 0
+        )
+
         selected_month = selectbox(
-            "Select the month", 
+            "Select the Month", 
             MONTHS, 
             dt.datetime.today().month - 1
         )
 
         selected_week = st.selectbox(
-            "Select the week",
-            options = range(1, 53),
-            index = dt.datetime.today().isocalendar().week - 1
+            "Select the Week",
+            options = range(1, 6),
+            index = math.ceil(dt.datetime.today().day / 7)
         )
+
 
     with tabs_component[0]: # Weekly Site
         from main.sites.weekly.report import weekly
-        weekly(data.copy(), selected_week)
+        weekly(data.copy(), select_year, selected_month, selected_week)
 
 
     with tabs_component[1]: # Monthly Site
