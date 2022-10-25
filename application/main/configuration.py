ee257537120c7dd6813b0f6e8a4724ba38e8940d
Dženan Džafić <dzeno.dzafic@windowslive.com>
2022-10-25 19:30:48 +0200

    Best practice changes

--- application/main/configuration.py
+++ application/main/configuration.py
@@ -13,4 +13,17 @@ class BaseConfiguration:
     """
 
     SCOPES = os.environ.get("SCOPES").strip().split(",")
-    CREDENTIALS_FILE_NAME = os.environ.get("GOOGLE_APPLICATION_CREDENTIALS")
+    # CREDENTIALS_FILE_NAME = os.environ.get("GOOGLE_APPLICATION_CREDENTIALS")
+
+    CREDENTALS = {
+        "type": os.environ.get("TYPE"),
+        "project_id": os.environ.get("PROJECT_ID"),
+        "private_key_id": os.environ.get("PRIVATE_KEY_ID"),
+        "private_key": os.environ.get("PRIVATE_KEY"),
+        "client_email": os.environ.get("CLIENT_EMAIL"),
+        "client_id": os.environ.get("CLIENT_ID"),
+        "auth_uri": os.environ.get("AUTH_URI"),
+        "token_uri": os.environ.get("TOKEN_URI"),
+        "auth_provider_x509_cert_url": os.environ.get("AUTH_PROVIDER_X509_CERT_URL"),
+        "client_x509_cert_url": os.environ.get("CLIENT_PROVIDER_X509_CERT_URL"),
+    }
