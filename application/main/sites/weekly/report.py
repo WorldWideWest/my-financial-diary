cc354f16218a170e9bd346d7bd74f0a1ad2fc057
Dženan Džafić <dzeno.dzafic@windowslive.com>
2022-10-26 07:11:43 +0200

    Started refactoring in the weekly module

--- application/main/sites/weekly/report.py
+++ application/main/sites/weekly/report.py
@@ -7,6 +7,8 @@ import plotly.express as px
 from main.utils.filter import Filter
 from main.utils.chart import Chart
 
+from main.static.components.data import MONTHS
+
 chart = Chart()
 filter = Filter()
 WEEK = dt.date.today().isocalendar().week
@@ -14,68 +16,60 @@ WEEK = dt.date.today().isocalendar().week
 hovertemplate = "<span style='color: #fff;'><span style='font-weight: 700;'>Day: %{x}</span>,<br>Amount: %{y:.2f} KM</span><extra></extra>"
 
 
-def transactions_view(transactions: pd.DataFrame, week: int) -> st.container:
-    transactions_container = st.container()
-    transactions_container.markdown(f"<h3 style='text-align:center;'>All Transactions in the Week { week } of the Year</h3>", unsafe_allow_html = True)
-    transactions_container.dataframe(transactions, use_container_width = True)
+class Weekly:
+        
+    def grouped_transactions_view(_self, transactions: pd.DataFrame, grouped_transactions: pd.DataFrame, week: int, month: int) -> st.columns:
+        grouped_transactions_container = st.columns([1, 3])
+        grouped_transactions_container[0].markdown(f"<h6 style='text-align:center;'>Grouped Transactions by Category for the Week { WEEK } of the Month</h6>", unsafe_allow_html = True)
+        grouped_transactions_container[0].dataframe(grouped_transactions)
+        
+        transactions = filter.group_by_date(transactions)
 
-    return transactions_container
-    
-def grouped_transactions_view(transactions: pd.DataFrame, grouped_transactions: pd.DataFrame, week: int) -> st.columns:
-    grouped_transactions_container = st.columns([1, 3])
-    grouped_transactions_container[0].markdown(f"<h6 style='text-align:center;'>Grouped Transactions by Category for the Week { WEEK } of the Month</h6>", unsafe_allow_html = True)
-    grouped_transactions_container[0].dataframe(grouped_transactions)
-    
-    transactions = filter.group_by_date(transactions)
+        figure = chart.line(transactions, "Date", "Amount")
+        figure.update_traces(hovertemplate = hovertemplate)
+        figure.layout.title = f"Spendings by Day for Week { week } in { MONTHS[month] }"
 
-    figure = chart.line(transactions, "Date", "Amount")
-    figure.update_traces(hovertemplate = hovertemplate)
-    figure.layout.title = f"Spendings by Day for the Week { week }"
+        grouped_transactions_container[1].plotly_chart(figure, use_container_width = True)
 
-    grouped_transactions_container[1].plotly_chart(figure, use_container_width = True)
+        return grouped_transactions_container
 
-    return grouped_transactions_container
+    def get_required_container(_self, transactions: pd.DataFrame):
+        actual_spendings = transactions["Amount"].sum()
+        
+        # Insted of 50, we want the value for the devidable categories devided by 4 (4 stands for the weeks in the month) 
+        
+        if(actual_spendings > 50):
+            return st.error(f"{ actual_spendings } / { 50 }")
+        return st.success(f"{ actual_spendings } / { 50 }")
 
-def get_required_container(transactions: pd.DataFrame):
-    actual_spendings = transactions["Amount"].sum()
-    
-    # Insted of 50, we want the value for the devidable categories devided by 4 (4 stands for the weeks in the month) 
-    
-    if(actual_spendings > 50):
-        return st.error(f"{ actual_spendings } / { 50 }")
-    return st.success(f"{ actual_spendings } / { 50 }")
+    def planned_spendings_info_view(_self, transactions: pd.DataFrame) -> st.columns:
+        planned_spending_container = st.columns([3, 1])
+        planned_spending_container[0].info(f"Planned and Actual spendings for the week { WEEK }")
 
-def planned_spendings_info_view(transactions: pd.DataFrame) -> st.columns:
-    planned_spending_container = st.columns([3, 1])
-    planned_spending_container[0].info(f"Planned and Actual spendings for the week { WEEK }")
+        with planned_spending_container[1]:
+            _self.get_required_container(transactions)
 
-    with planned_spending_container[1]:
-        get_required_container(transactions)
+        return planned_spending_container
 
-    return planned_spending_container
+    def report(_self, data: pd.DataFrame, year: int = None, month: int = None, week: int = None) -> pd.DataFrame:
+        """
+            Make sure that the data is a copy of the dataframe and not 
+            the original object that is fetched from Google Sheets API.
 
-def weekly(data: pd.DataFrame, year: int = None, month: int = None, week: int = None) -> pd.DataFrame:
-    """
-        Make sure that the data is a copy of the dataframe and not 
-        the original object that is fetched from Google Sheets API.
+            To do that on passing the `data` argument to the function add
+            the pandas method copy() in front of it to make sure that the 
+            DataFrame was in fact copied.
+        """
+        
 
-        To do that on passing the `data` argument to the function add
-        the pandas method copy() in front of it to make sure that the 
-        DataFrame was in fact copied.
-    """
-    
-    filtered = data.copy()
+        transactions = filter.filter_by_date(data, year = year, month = month, week = week)
+        grouped = filter.group_by_category(transactions)
 
-    filtered["Date"] = pd.to_datetime(filtered["Date"], format="%m/%d/%Y")
+        with st.expander(f"All Transactions for Week { week } in { MONTHS[month] } ", expanded = False):
+            st.dataframe(transactions, use_container_width = True)
 
-    transactions = filter.filter_by_date(filtered, year = year, month = month, week = week)
-    grouped = filter.group_by_category(transactions)
+        _self.grouped_transactions_view(transactions, grouped, week, month)
 
-    with st.expander("Transactions", expanded = True):
-        transactions_view(transactions, week)
-
-    grouped_transactions_view(transactions, grouped, week)
-
-    planned_spendings_info_view(transactions)
+        _self.planned_spendings_info_view(transactions)
 
 
