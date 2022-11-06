ae3bed14b6ba52a5cd3f9964f6eb291275a85a95
Dženan Džafić <dzeno.dzafic@windowslive.com>
2022-11-06 14:27:10 +0100

    Updating the dataframe for the not devidable stuff

--- application/main/utils/filter.py
+++ application/main/utils/filter.py
@@ -42,8 +42,43 @@ class Filter(object):
         return data[(data["Date"].dt.month == month)]
 
     @st.experimental_memo()
-    def planned_monthly_data(_self, data: pd.DataFrame, month: str, devidable: bool) -> pd.DataFrame:
-        if devidable:
-            return data[data["Devidable"] == "TRUE"] [["Categories", month]]
-        return data[data["Devidable"] == "FALSE"] [["Categories", month]]
+    def spendings_statistics(_self, planned: pd.DataFrame, transactions: pd.DataFrame, month: str, devidable: bool) -> pd.DataFrame:
+
+        """
+            `spendings_statistics` - method takes as arguments:
+                - planned - a DataFrame that includes the planned spendings by category for a given month
+                - transactions - is a DataFrame with all transactions for the month that the data was planned for
+                - month - string representation of a month
+                - devidable - is a column in the planned DataFrame where the categories are splited into devidable (TRUE) and not devidable (FALSE), those who are not devidable will only show up in the monthly planning and those who are devidable will show up in the weekly preview.
+        """
+
+        data = planned.copy()
+        data["Devidable"] = data["Devidable"].replace({"TRUE": True, "FALSE": False})
+
+        if not isinstance(_self.try_get_column(data, month), IndexError):
+            data = data[data["Devidable"] == devidable]
+            data = data[["Categories", month]]
+
+            data["Spent"], total = 0, 0
+
+            for category in planned["Categories"]:
+                total = transactions[transactions["Category"] == category]["Amount"].sum()
+                index = data[data["Categories"] == category].index
+                
+                if not isinstance(_self.try_get_index(index), IndexError):
+                    index = index[0]
+                    data.loc[index, "Spent"] = total
         
+        return data
+
+    def try_get_index(_self, index):
+        try:
+            index[0:1][0]
+        except IndexError as e:
+            return e
+
+    def try_get_column(_self, data: pd.DataFrame, month: str):
+        try:
+            data[month]
+        except IndexError as e:
+            return e
