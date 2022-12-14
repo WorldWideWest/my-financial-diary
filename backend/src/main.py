41efedae80a422a0c379d37b8e1cbdc4574c933e
Dženan Džafić <dzeno.dzafic@windowslive.com>
2022-12-14 20:13:52 +0100

    Fixed issue with postgres not starting and added it to docs in README, also created a DB session object and Base object for generating models

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,19 +1,14 @@
 from fastapi import FastAPI
 
 from src.configuration import BaseConfiguration
-from src.database import engine
+from src.database import engine, LocalSession
 
 app = FastAPI()
 
 @app.get("/")
 async def get_world() -> dict:
-
-    
-    engine
-
     context = {
         "message": "Hello World", 
         "author": "Dzeno",
     }
-
     return context
